3060e7505cae9c5969301cb41f98d16154cc8c20
Eduard Ereza Martínez <eduard@eduardereza.com>
2015-03-12 10:51:59 +0100

    Don't use an alarm to launch the intent, this is what ACRA does and it should work better everywhere.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ How to use
 
 **This is just a sample.** Take a look at it and implement it in your project as needed, making any tweaks you want in order to fit your needs.
 
-This sample relies on the `Thread.setDefaultUncaughtExceptionHandler` method and `AlarmManager` to start a new `PendingIntent` to your activity, then kills the current process. Look at the code and you will see how it works (it's all in the application class).
+This sample relies on the `Thread.setDefaultUncaughtExceptionHandler` method and launches a new intent to your error activity, then kills the current process. Look at the code and you will see how it works (it's all in the application class).
 
 The sample passes the stack trace to the `ErrorActivity`, but you can customize it as you wish. Keep in mind that the `Intent` limits are around 1MB and you may get a `TransactionTooLargeException`. Reduce the passed data to fit those limits.
 
@@ -17,7 +17,15 @@ If you already have ACRA, Crashlytics or any similar library bundled into your a
 Disclaimers
 =====================
 
-I have tested this on devices with Android 4.0.3, 4.4.4 and L preview. It works fine on those devices, but I haven't applied it to any production code, so be aware that errors may arise.
+I have tested this on the following devices:
+
+* HTC One M8, Android 4.4
+* Samsung Galaxy S4 mini (GT-I9195), Android 4.4
+* Samsung Galaxy A3 (SM-A300FU), Android 4.4
+* Nexus 5, Android 5.0
+* Nexus 5, Android 5.1
+
+It works fine on those devices, but I haven't applied it to any production code, so be aware that errors may arise.
 
 * There is no guarantee that this will work on your device
 * There is no guarantee that this will work on Android <4.0.3, but maybe it does (I haven't tried it, I currently develop for API 15+)
--- app/src/main/java/cat/ereza/example/customactivityoncrash/CrashableApplication.java
+++ app/src/main/java/cat/ereza/example/customactivityoncrash/CrashableApplication.java
@@ -1,65 +1,102 @@
 package cat.ereza.example.customactivityoncrash;
 
-import android.app.AlarmManager;
+import android.app.Activity;
 import android.app.Application;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.ref.WeakReference;
 
 public class CrashableApplication extends Application {
 
     public final static String TAG = "CRASHABLE";
-    public final static int EXTRA_TIME_TO_WAIT_MILLIS = 200;
+
+    private WeakReference<Activity> lastActivityCreated = new WeakReference<>(null);
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        //We define a default exception handler that just kills the process so it can be called from Crashlytics/ACRA
+        //We define a default exception handler that does what we want so it can be called from Crashlytics/ACRA
         Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
             @Override
-            public void uncaughtException(Thread thread, Throwable throwable) {
-                //You MUST do this to finish the process, otherwise you will be stuck in limbo.
-                Log.d(TAG, "Finishing process!");
-                android.os.Process.killProcess(android.os.Process.myPid());
-                System.exit(0);
+            public void uncaughtException(Thread thread, final Throwable throwable) {
+                Log.d(TAG, "App has crashed, executing UncaughtExceptionHandler", throwable);
+
+                new Thread() {
+                    @Override
+                    public void run() {
+                        final Intent intent = new Intent(CrashableApplication.this, ErrorActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                        //Change this to anything you need to treat the error and pass any parameters you like...
+                        StringWriter sw = new StringWriter();
+                        PrintWriter pw = new PrintWriter(sw);
+                        throwable.printStackTrace(pw);
+                        intent.putExtra(ErrorActivity.EXTRA_EXCEPTION, sw.toString());
+
+                        startActivity(intent);
+                        final Activity lastActivity = lastActivityCreated.get();
+                        if (lastActivity != null) {
+                            //We finish the activity, this solves a bug, see https://github.com/ACRA/acra/issues/42
+                            lastActivity.finish();
+                            lastActivityCreated.clear();
+                        }
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                        System.exit(10);
+                    }
+                }.start();
+            }
+        });
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                if (!(activity instanceof ErrorActivity)) {
+                    // Copied from ACRA:
+                    // Ignore ErrorActivity because we want the last
+                    // application Activity that was started so that we can
+                    // explicitly kill it off.
+                    lastActivityCreated = new WeakReference<>(activity);
+                }
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+                //Do nothing
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+                //Do nothing
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+                //Do nothing
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+                //Do nothing
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+                //Do nothing
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                //Do nothing
             }
         });
 
         //Initialize your error handlers as normal, they will most likely keep a reference to the original exception handler
         //i.e., ACRA.init(this);
         //or Crashlytics.start(this);
-
-        //Keep a reference to the original handler to use it later, after handling the exception ourselves
-        final Thread.UncaughtExceptionHandler originalHandler = Thread.getDefaultUncaughtExceptionHandler();
-
-        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-            @Override
-            public void uncaughtException(Thread thread, Throwable throwable) {
-                Log.d(TAG, "Crashed, executing UncaughtExceptionHandler");
-
-                Intent intent = new Intent(CrashableApplication.this, ErrorActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_HISTORY);
-
-                //Change this to anything you need to treat the error and pass any parameters you like...
-                StringWriter sw = new StringWriter();
-                PrintWriter pw = new PrintWriter(sw);
-                throwable.printStackTrace(pw);
-                intent.putExtra(ErrorActivity.EXTRA_EXCEPTION, sw.toString());
-
-                //We define an alarm to launch our new intent in very little time
-                //EXTRA_TIME_TO_WAIT_MILLIS is necessary to avoid launching it while it still has not finished (I know it's ugly...)
-                PendingIntent pendingIntent = PendingIntent.getActivity(CrashableApplication.this, 0, intent, 0);
-                AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                alarmManager.set(AlarmManager.RTC, System.currentTimeMillis() + EXTRA_TIME_TO_WAIT_MILLIS, pendingIntent);
-
-                //Call the original handler which will handle the exception as normal and then launch our own finishing handler (avoiding the "App has stopped" dialog)
-                originalHandler.uncaughtException(thread, throwable);
-            }
-        });
     }
 }
